8fc41991ecc49ca1add2634c6a2c5eb0069b979a
Jonas Liechti <jonas.i.liechti@protonmail.ch>
2021-03-17 01:44:28 +0100

    started with examples

--- examples/alluvial_basic.py
+++ examples/alluvial_basic.py
@@ -1,10 +1,82 @@
 """
-==================
-The Alluvial class
-==================
+=================================
+Alluvial Demo - Network structure
+=================================
 
-Demonstrate the Alluvial class by means of a simple example.
+Draw Alluvial diagram to reflect changes in network structure.
 """
+import matplotlib.pyplot as plt
+import numpy as np
+# from matplotlib.alluvial import Alluvial
+from pyalluv import Alluvial
+
+# Consider a small network of 4 nodes with membership lists at 3 point in time:
+memberships = [[0, 1, 1, 2],  # t1
+               [3, 0, 1, 2],  # t2
+               [1, 0, 1, 1]]  # t3
+
+
+# Create an alluvial diagram based on the memberships
+alluv = Alluvial.from_memberships(memberships, layout='top',
+                                  x=['t1', 't2', 't3'], width=0.2)
+# Tell alluvial to determine the layout and draw the diagrams
+alluv.finish()
+
+plt.show()
+
+#############################################################################
+# =======================
+# Alluvial Demo - Layouts
+# =======================
+#
+# Draw Alluvial diagrams with different layouts
+#
+import matplotlib.pyplot as plt
+import numpy as np
+# from matplotlib.alluvial import Alluvial
+from pyalluv import Alluvial
+
+memberships = [[0, 1, 1, 2],  # t1
+               [3, 0, 1, 2],  # t2
+               [1, 0, 1, 1]]  # t3
+
+layouts = ['centered', 'bottom', 'top']
+y_offsets = [0, 3, -3]
+alluv = Alluvial(x=['t1', 't2', 't3'], width=0.4, fc=None)
+for layout, yoff in zip(layouts, y_offsets):
+    # Create an alluvial diagram based on the memberships using a centered layout
+    alluv.add_from_memberships(memberships, layout=layout, yoff=yoff)
+# TODO: Adding labels for sub-diagrams
+# Tell alluvial to determine the layout and draw the diagrams
+alluv.finish()
+
+plt.show()
+
+#############################################################################
+#
+# ------------
+#
+# References
+# """"""""""
+#
+# The use of the following functions, methods, classes and modules is shown
+# in this example:
+
+import matplotlib
+matplotlib.alluvial
+matplotlib.alluvial.Alluvial
+matplotlib.alluvial.Alluvial.from_memberships
+matplotlib.alluvial.Alluvial.finish
+matplotlib.alluvial.Alluvial.add_from_memberships
+
+###################
+###################
+###################
+
+
+
+
+
 from copy import deepcopy
 from matplotlib import pyplot as plt
 # import matplotlib.gridspec as gridspec
